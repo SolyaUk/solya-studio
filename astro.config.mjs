15c74f25cddf61ced0543b251262ad3604ae762e
SolyaUK <solya.uk@gmail.com>
2026-03-15 23:13:21 +0200

    feat: hero section complete

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://solya.studio',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [sitemap()]
 });
